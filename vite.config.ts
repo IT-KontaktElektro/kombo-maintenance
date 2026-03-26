36a66f41cce5f2daa55842ee8093072ed7cf363a
Haaz Gréta <haazgretike67@gmail.com>
2026-03-26 09:52:56 +0100

    fix: compression implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression';
 import svgr from '@svgr/rollup';
 
 export default defineConfig({
   plugins: [
     react(),
-    svgr()
+    svgr(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
   ],
 
   build: {
