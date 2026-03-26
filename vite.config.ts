5ad734d98c18197c30b8045b07ceb822dd2bb3a2
Haaz Gréta <haazgretike67@gmail.com>
2026-03-26 09:20:49 +0100

    fix: vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+  ],
+
+  build: {
+    target: 'esnext',
+    sourcemap: false,
+    minify: 'esbuild',
+    outDir: '../wwwroot',   // ✅ build output goes here
+    emptyOutDir: true,   // ✅ cleans wwwroot before each build
+  }
 })
